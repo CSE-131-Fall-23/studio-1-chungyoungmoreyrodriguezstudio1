558b5a3641dd146c050ee33e5359b224c5c42306
brisarodriguez <brisarodriguez@localhost>
2023-09-06 14:10:14 -0500

    sep.6

--- src/studio1/Hello.java
+++ src/studio1/Hello.java
@@ -7,6 +7,9 @@ public class Hello {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
+		System.out.println("What is your name?");
+		String name = in.nextLine();
+		
 		System.out.println("How are you feeling today?");
 		String mood = in.nextLine();
 		
